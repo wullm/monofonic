28e072dea31b2b4d2ce1296654e0d7ae0a154fde
Oliver Hahn <oliverjhahn@gmail.com>
2019-05-20 17:23:21 +0200

    fixed mixup between time coeffs and 3a 3b terms

--- src/grid_fft.cc
+++ src/grid_fft.cc
@@ -564,9 +564,7 @@ void Grid_FFT<data_t>::Write_PowerSpectrum( std::string ofname )
     {
 #endif
         std::ofstream ofs( ofname.c_str());
-        std::size_t numcells = size(0) * size(1) * size(2);
 
-        //ofs << "# a = " << aexp << std::endl;
         ofs << "# " << std::setw(14) << "k" << std::setw(16) << "P(k)" << std::setw(16) << "err. P(k)"
             << std::setw(16) <<"#modes" << "\n";
 
