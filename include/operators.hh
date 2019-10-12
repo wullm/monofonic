cbec8c8f030d59fd8a6d90cb9a18bdf4c9a9caed
Oliver Hahn <oliverjhahn@gmail.com>
2019-10-12 15:06:49 +0200

    inlined global lambdas, requires upgrading to C++17

--- include/operators.hh
+++ include/operators.hh
@@ -1,9 +1,9 @@
 #pragma once
 
 namespace op{
-auto assign_to = [](auto &g){return [&](auto i, auto v){ g[i] = v; };};
-auto add_to = [](auto &g){return [&](auto i, auto v){ g[i] += v; };};
-auto add_twice_to = [](auto &g){return [&](auto i, auto v){ g[i] += 2*v; };};
-auto subtract_from = [](auto &g){return [&](auto i, auto v){ g[i] -= v; };};
-auto subtract_twice_from = [](auto &g){return [&](auto i, auto v){ g[i] -= 2*v; };};
+inline auto assign_to = [](auto &g){return [&](auto i, auto v){ g[i] = v; };};
+inline auto add_to = [](auto &g){return [&](auto i, auto v){ g[i] += v; };};
+inline auto add_twice_to = [](auto &g){return [&](auto i, auto v){ g[i] += 2*v; };};
+inline auto subtract_from = [](auto &g){return [&](auto i, auto v){ g[i] -= v; };};
+inline auto subtract_twice_from = [](auto &g){return [&](auto i, auto v){ g[i] -= 2*v; };};
 }
