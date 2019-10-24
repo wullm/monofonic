4de579ca7831f44897ba54406f97391cb45da2c0
Oliver Hahn <oliverjhahn@gmail.com>
2019-10-24 17:12:43 +0200

    revert back to C++14 by replacing inline lambdas with template funcs

--- include/operators.hh
+++ include/operators.hh
@@ -1,9 +1,26 @@
 #pragma once
 
 namespace op{
-inline auto assign_to = [](auto &g){return [&](auto i, auto v){ g[i] = v; };};
-inline auto add_to = [](auto &g){return [&](auto i, auto v){ g[i] += v; };};
-inline auto add_twice_to = [](auto &g){return [&](auto i, auto v){ g[i] += 2*v; };};
-inline auto subtract_from = [](auto &g){return [&](auto i, auto v){ g[i] -= v; };};
-inline auto subtract_twice_from = [](auto &g){return [&](auto i, auto v){ g[i] -= 2*v; };};
+
+template< typename grid>
+inline auto assign_to( grid& g ){return [&](auto i, auto v){ g[i] = v; };}
+
+template< typename grid>
+inline auto add_to( grid& g ){return [&](auto i, auto v){ g[i] += v; };}
+
+template< typename grid>
+inline auto add_twice_to( grid& g ){return [&](auto i, auto v){ g[i] += 2*v; };}
+
+template< typename grid>
+inline auto subtract_from( grid& g ){return [&](auto i, auto v){ g[i] -= v; };}
+
+template< typename grid>
+inline auto subtract_twice_from( grid& g ){return [&](auto i, auto v){ g[i] -= 2*v; };}
+
+// above template functions can be written as C++17 inline lambdas... but we're using C++14...
+// inline auto assign_to = [](auto &g){return [&](auto i, auto v){ g[i] = v; };};
+// inline auto add_to = [](auto &g){return [&](auto i, auto v){ g[i] += v; };};
+// inline auto add_twice_to = [](auto &g){return [&](auto i, auto v){ g[i] += 2*v; };};
+// inline auto subtract_from = [](auto &g){return [&](auto i, auto v){ g[i] -= v; };};
+// inline auto subtract_twice_from = [](auto &g){return [&](auto i, auto v){ g[i] -= 2*v; };};
 }
