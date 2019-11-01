c593a7067d72d5b31d7b020c8d465644badeb415
Oliver Hahn <oliverjhahn@gmail.com>
2019-11-01 04:49:40 +0100

    cleanup particle generator

--- include/particle_generator.hh
+++ include/particle_generator.hh
@@ -16,7 +16,7 @@ const std::vector< std::vector<vec3<real_t>> > lattice_shifts =
     // first shift must always be zero! (otherwise set_positions and set_velocities break)
     /* SC : */ {{0.0,0.0,0.0}},
     /* BCC: */ {{0.0,0.0,0.0},{0.5,0.5,0.5}},
-    /* FCC: */ {{0.0,0.0,0.0},{0.5,0.5,0.0},{0.5,0.0,0.5},{0.0,0.5,0.5}},
+    /* FCC: */ {{0.0,0.0,0.0},{0.0,0.5,0.5},{0.5,0.0,0.5},{0.5,0.5,0.0}},
     /* RSC: */ {{0.0,0.0,0.0},{0.0,0.0,0.5},{0.0,0.5,0.0},{0.0,0.5,0.5},{0.5,0.0,0.0},{0.5,0.0,0.5},{0.5,0.5,0.0},{0.5,0.5,0.5}},
 };
 
@@ -72,131 +72,6 @@ void set_velocities( container& particles, lattice lattice_type, int idim, field
             field.shift_field( shift.x, shift.y, shift.z );
         }
         auto ipcount0 = ishift * num_p_in_load;
-        for( size_t i=0,ipcount=ipcount0; i<field.size(0); ++i ){
-            for( size_t j=0; j<field.size(1); ++j){
-                for( size_t k=0; k<field.size(2); ++k){
-                    particles.set_vel( ipcount++, idim, field.relem(i,j,k) );
-                }
-            }
-        }
-    }
-}
-
-
-///// deprecated code below ////////////////////////////////////////////////////
-
-// invalidates field, phase shifted to unspecified position after return
-template<typename field_t>
-void set_positions_old( container& particles, lattice lattice_type, int idim, real_t lunit, field_t& field )
-{
-    const size_t num_p_in_load = field.local_size();
-
-    for( size_t i=0,ipcount=0; i<field.size(0); ++i ){
-        for( size_t j=0; j<field.size(1); ++j){
-            for( size_t k=0; k<field.size(2); ++k){
-                auto pos = field.template get_unit_r<real_t>(i,j,k);
-                particles.set_pos( ipcount++, idim, pos[idim]*lunit + field.relem(i,j,k) );
-            }
-        }
-    }
-
-    if( lattice_type == particle::lattice_bcc ){
-        field.shift_field( 0.5, 0.5, 0.5 );
-        auto ipcount0 = num_p_in_load;
-        for( size_t i=0,ipcount=ipcount0; i<field.size(0); ++i ){
-            for( size_t j=0; j<field.size(1); ++j){
-                for( size_t k=0; k<field.size(2); ++k){
-                    auto pos = field.template get_unit_r_shifted<real_t>(i,j,k,0.5,0.5,0.5);
-                    particles.set_pos( ipcount++, idim, pos[idim]*lunit + field.relem(i,j,k) );
-                }
-            }
-        }
-    }
-    else if( lattice_type == particle::lattice_fcc ){ 
-        // 0.5 0.5 0.0
-        field.shift_field( 0.5, 0.5, 0.0 );
-        auto ipcount0 = num_p_in_load;
-        for( size_t i=0,ipcount=ipcount0; i<field.size(0); ++i ){
-            for( size_t j=0; j<field.size(1); ++j){
-                for( size_t k=0; k<field.size(2); ++k){
-                    auto pos = field.template get_unit_r_shifted<real_t>(i,j,k,0.5,0.5,0.0);
-                    particles.set_pos( ipcount++, idim, pos[idim]*lunit + field.relem(i,j,k) );
-                }
-            }
-        }
-        // 0.0 0.5 0.5
-        field.shift_field( -0.5, 0.0, 0.5 );
-        ipcount0 = 2*num_p_in_load;
-        for( size_t i=0,ipcount=ipcount0; i<field.size(0); ++i ){
-            for( size_t j=0; j<field.size(1); ++j){
-                for( size_t k=0; k<field.size(2); ++k){
-                    auto pos = field.template get_unit_r_shifted<real_t>(i,j,k,0.0,0.5,0.5);
-                    particles.set_pos( ipcount++, idim, pos[idim]*lunit + field.relem(i,j,k) );
-                }
-            }
-        }
-        // 0.5 0.0 0.5
-        field.shift_field( 0.5, -0.5, 0.0 );
-        ipcount0 = 3*num_p_in_load;
-        for( size_t i=0,ipcount=ipcount0; i<field.size(0); ++i ){
-            for( size_t j=0; j<field.size(1); ++j){
-                for( size_t k=0; k<field.size(2); ++k){
-                    auto pos = field.template get_unit_r_shifted<real_t>(i,j,k,0.5,0.0,0.5);
-                    particles.set_pos( ipcount++, idim, pos[idim]*lunit + field.relem(i,j,k) );
-                }
-            }
-        }
-    }
-}
-
-template<typename field_t>
-void set_velocities_old( container& particles, lattice lattice_type, int idim, field_t& field )
-{
-    const size_t num_p_in_load = field.local_size();
-
-    for( size_t i=0,ipcount=0; i<field.size(0); ++i ){
-        for( size_t j=0; j<field.size(1); ++j){
-            for( size_t k=0; k<field.size(2); ++k){
-                particles.set_vel( ipcount++, idim, field.relem(i,j,k) );
-            }
-        }
-    }
-
-    if( lattice_type == particle::lattice_bcc ){
-        field.shift_field( 0.5, 0.5, 0.5 );
-        auto ipcount0 = num_p_in_load;
-        for( size_t i=0,ipcount=ipcount0; i<field.size(0); ++i ){
-            for( size_t j=0; j<field.size(1); ++j){
-                for( size_t k=0; k<field.size(2); ++k){
-                    particles.set_vel( ipcount++, idim, field.relem(i,j,k) );
-                }
-            }
-        }
-    }
-    else if( lattice_type == particle::lattice_fcc ){ 
-        // 0.5 0.5 0.0
-        field.shift_field( 0.5, 0.5, 0.0 );
-        auto ipcount0 = num_p_in_load;
-        for( size_t i=0,ipcount=ipcount0; i<field.size(0); ++i ){
-            for( size_t j=0; j<field.size(1); ++j){
-                for( size_t k=0; k<field.size(2); ++k){
-                    particles.set_vel( ipcount++, idim, field.relem(i,j,k) );
-                }
-            }
-        }
-        // 0.0 0.5 0.5
-        field.shift_field( -0.5, 0.0, 0.5 );
-        ipcount0 = 2*num_p_in_load;
-        for( size_t i=0,ipcount=ipcount0; i<field.size(0); ++i ){
-            for( size_t j=0; j<field.size(1); ++j){
-                for( size_t k=0; k<field.size(2); ++k){
-                    particles.set_vel( ipcount++, idim, field.relem(i,j,k) );
-                }
-            }
-        }
-        // 0.5 0.0 0.5
-        field.shift_field( 0.5, -0.5, 0.0 );
-        ipcount0 = 3*num_p_in_load;
         for( size_t i=0,ipcount=ipcount0; i<field.size(0); ++i ){
             for( size_t j=0; j<field.size(1); ++j){
                 for( size_t k=0; k<field.size(2); ++k){
