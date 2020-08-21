d5493ca70a4748ef636525b413063a33c39036a9
Oliver Hahn <oliverjhahn@gmail.com>
2020-08-21 17:03:33 +0200

    added license info to each file

--- include/particle_generator.hh
+++ include/particle_generator.hh
@@ -1,10 +1,19 @@
-/*******************************************************************\
- particle_generator.hh - This file is part of MUSIC2 -
- a code to generate initial conditions for cosmological simulations 
- 
- CHANGELOG (only majors, for details see repo):
-    10/2019 - Oliver Hahn - first implementation
-\*******************************************************************/
+// This file is part of monofonIC (MUSIC2)
+// A software package to generate ICs for cosmological simulations
+// Copyright (C) 2020 by Oliver Hahn
+// 
+// monofonIC is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// monofonIC is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #include <math/vec3.hh>
