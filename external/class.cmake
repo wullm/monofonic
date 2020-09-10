143e5382a9eea1c825916f5e96e797d986fe1e2b
Oliver Hahn <oliverjhahn@gmail.com>
2020-09-10 20:15:10 +0200

    added progress verbosity to cmake fetch to avoid impression that cmake hangs while it clones CLASS

--- external/class.cmake
+++ external/class.cmake
@@ -4,10 +4,13 @@ FetchContent_Declare(
     class
     GIT_REPOSITORY https://github.com/ohahn/class_public.git
     GIT_TAG master
+    GIT_PROGRESS TRUE
+    USES_TERMINAL_DOWNLOAD TRUE   # <---- this is needed only for Ninja
 )
 
 FetchContent_GetProperties(class)
 if(NOT class_POPULATED)
+    set(FETCHCONTENT_QUIET OFF)
     FetchContent_Populate(class)
     add_subdirectory(${class_SOURCE_DIR} ${class_BINARY_DIR})
 endif()
