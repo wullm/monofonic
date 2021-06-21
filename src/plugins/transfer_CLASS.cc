6890d6b5f48bad1068031b68057288936b7a56d0
Willem Elbers <willem.h.elbers@durham.ac.uk>
2021-06-21 04:09:23 +0100

    Remove sampling precision parameter

--- src/plugins/transfer_CLASS.cc
+++ src/plugins/transfer_CLASS.cc
@@ -141,9 +141,6 @@ private:
     add_class_parameter("transfer_neglect_delta_k_S_e", 0.13);
     add_class_parameter("delta_l_max", 1000);
 
-    // fine sampling along the time dimension to allow accurate interpolation
-    add_class_parameter("perturb_sampling_stepsize", 0.005);
-
     int class_verbosity = 0;
 
     add_class_parameter("background_verbose", class_verbosity);
