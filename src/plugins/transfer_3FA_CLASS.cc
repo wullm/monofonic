f88e3ec88f58e126da8b8919c885201092755456
wullm <willem.h.elbers@durham.ac.uk>
2021-08-22 20:53:20 +0100

    Simplify

--- src/plugins/transfer_3FA_CLASS.cc
+++ src/plugins/transfer_3FA_CLASS.cc
@@ -490,8 +490,6 @@ public:
         // compute the mass-weighted average
         dcb = f_b * db[i] + (1.0 - f_b) * dc[i];
         tcb = f_b * tb[i] + (1.0 - f_b) * tc[i];
-        // dm[i] = f_nu_nr_0 * dn[i] + (1.0 - f_nu_nr_0) * dcb;
-        // tm[i] = f_nu_nr_0 * tn[i] + (1.0 - f_nu_nr_0) * tcb;
 
         if (bCDMBaryonMatterOnly) {
             dm[i] = dcb;
