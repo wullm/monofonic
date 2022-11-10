a6e148ed0005012a85dbd46b591c18e172f91ab5
Oliver Hahn <oliverjhahn@gmail.com>
2022-11-10 19:59:22 +0100

    fixed compiler warning

--- src/plugins/output_grafic2.cc
+++ src/plugins/output_grafic2.cc
@@ -212,7 +212,7 @@ void grafic2_output_plugin::write_grid_data(const Grid_FFT<real_t> &g, const cos
         unlink(file_name.c_str());
     }
 
-    std::ofstream *pofs;
+    std::ofstream *pofs = nullptr;
 
     // write header or seek to end of file
     if (CONFIG::MPI_task_rank == 0)
